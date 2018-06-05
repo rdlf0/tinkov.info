53fc6eb8316aa720f071b663ad569764a6099114
Radoslav Stoyanov <r.stoyanov@outlook.com>
2018-06-05 00:49:05 -0400

    Added Ridley Grotesk font.

--- DATA/fonts.ts
+++ DATA/fonts.ts
@@ -43,5 +43,45 @@ export const fonts: Font[] = [
                 style: 'Light'
             }
         }
+    },
+    {
+        id: 2,
+        name: 'Ridley Grotesk',
+        slug: 'ridley-grotesk',
+        directory: 'RidleyGrotesk',
+        alternatives: [
+            'sans-serif'
+        ],
+        styles: [
+            'Thin',
+            'ThinItalic',
+            'UltraLight',
+            'UltraLightItalic',
+            'Light',
+            'LightItalic',
+            'Regular',
+            'RegularItalic',
+            'Medium',
+            'MediumItalic',
+            'SemiBold',
+            'SemiBoldItalic',
+            'Bold',
+            'BoldItalic',
+            'ExtraBold',
+            'ExtraBoldItalic',
+            'Black',
+            'BlackItalic'
+        ],
+        stylesFree: [
+            'Light',
+            'SemiBold'
+        ],
+        list: {
+            preview: {
+                text: 'Gane/4 30.5ml',
+                transform: 'none',
+                style: 'Regular'
+            }
+        }
     }
 ];
