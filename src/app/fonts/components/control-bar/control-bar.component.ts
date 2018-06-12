33f2b1182a2d9b8b75e0f9cc16d6922f53edd40b
Radoslav Stoyanov <r.stoyanov@outlook.com>
2018-06-12 17:13:55 -0400

    Updated the selector of the control bar.

--- src/app/fonts/components/control-bar/control-bar.component.ts
+++ src/app/fonts/components/control-bar/control-bar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { WidgetService } from '../../services/control-bar/widget.service';
 
 @Component({
-    selector: 'app-control-bar',
+    selector: 'app-fonts-control-bar',
     templateUrl: './control-bar.component.html',
     styleUrls: ['./control-bar.component.css'],
     providers: [WidgetService]
